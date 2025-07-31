11918c61440eed6552c61ddbea96d4aade88383e
yun <yun@sesac.com>
2025-07-31 21:48:49 +0900

    인덱스html이름을포스트_리스트로리펙토링

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from .views import PostListView
 
 urlpatterns = [
+    path('', PostListView.as_view(), name='post-list'),
     path('<int:pk>/', views.detail, name='detail'),
-    path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
-from .models import Post   	#한줄추가. Post 모델 임포트하는 과정
+from django.views.generic import ListView
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')	#한줄추가. 인덱스함수에서 Post.objects.all()로 모든 Post 레코드를 가져와서 posts에 저장
-
-    return render(
-	request,
-	'blog/index.html',
-        {
-        	'posts': posts,  # { 'posts': posts, } 추가하기
-        }
-)
 
 def detail(request, pk):
     post = Post.objects.get(pk=pk)
