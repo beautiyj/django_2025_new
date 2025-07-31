a1f784b24f232c798f8902751b96d57e854f2039
yun <yun@sesac.com>
2025-07-31 18:18:13 +0900

    블로그모델파이에서 자동으로 작성시각 수정시각 적용 완

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# 이 밑에 2줄을 적는다
+from .models import Post
 
-# Register your models here.
+admin.site.register(Post)
+
+#해당 두 줄은 관리자페이지에 포스트 모델을 등록하는 과정임!
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+# Post 모델은 models 모듈의 Model 클래스를 확장해서 만든 파이썬 클래스.
 from django.db import models
 
-# Create your models here.
+class Post(models.Model) :
+	title = models.CharField(max_length=30)    # title 필드는 CharField (문자열필드정의 클래스), 문자필드, 최대 길이(필수설정) 30으로 설정
+	content = models.TextField()			   # content 필드는 TextField (텍스트저장 문자열필드클래스), 문자열의 길이 제한 없도록(본문내용이니까)
+
+	created_at = models.DateTimeField(auto_now_add=True, null=True)	   # created_at 필드는 DateTimeField(날짜+시간저장 필드클래스),2025-07-30 14:32:00 등의형식으로저장됨
+								   # 해당 필드에는 작성된 시간 등 저장 가능,
+								   # auto_now_add=True 같은 옵션을 주면 자동으로 생성 시간 저장도 가능.
+	updated_at = models.DateTimeField(auto_now_add=True, null=True)  # 해당줄추가
+	#author : 추후 작성 예정
+	def __str__(self):
+		return f' [ {self.pk} ]{self.title}'  # 두줄추가하기.
