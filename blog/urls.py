109fc9262491f323cceb8c04aeecd24c5551d035
yun <yun@sesac.com>
2025-07-31 18:35:08 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # 이제 이 부분의 공백을 채워야함!
+    path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post   	#한줄추가. Post 모델 임포트하는 과정
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk')	#한줄추가. 인덱스함수에서 Post.objects.all()로 모든 Post 레코드를 가져와서 posts에 저장
+
+    return render(
+	request,
+	'blog/index.html',
+        {
+        	'posts': posts,  # { 'posts': posts, } 추가하기
+        }
+)
