1ae463a3aecf5ccde1a596c4dbe57d3d30f7fee4
yun <yun@sesac.com>
2025-08-01 16:56:21 +0900

    create 글쓰기페이지 만들기완

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+#blog/forms.py
+from django import forms
+from blog.models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title',
+                  'author',
+                  'content',
+                  'category',
+                  'uploaded_image',
+                  'uploaded_file' ]
